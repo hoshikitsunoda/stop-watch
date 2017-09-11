d5ea299a2f4ab262ee5ff57675ffc9b4dc07f345
Hoshiki Tsunoda <liquidohr@gmail.com>
2017-09-11 10:51:28 -0700

    Fixed the double speed issue

--- main.js
+++ main.js
@@ -1,18 +1,28 @@
 /* eslint-disable no-unused-vars */
 var $time = document.querySelector('#time')
 var $start = document.querySelector('#start')
+var $stop = document.querySelector('#stop')
+var $reset = document.querySelector('#reset')
+var $limit = document.querySelector('#limit')
+var $set = document.querySelector('#set')
 
 var timer = {
   seconds: 0,
   id: null,
+  limit: $limit.value
 }
 
 function start() {
-  timer.id = setInterval(function () {
-    timer.seconds++
-    render()
-    console.log('counting...')
-  }, 1000)
+  if(timer.id === null){
+    timer.id = setInterval(function () {
+      timer.seconds++
+      render()
+    }, 1000)
+  }
+}
+
+function stop() {
+  clearInterval(timer.id)
 }
 
 function render() {
@@ -20,3 +30,7 @@ function render() {
 }
 
 $start.addEventListener('click', start)
+
+$stop.addEventListener('click', stop)
+
+$reset.addEventListener('click', reset)
